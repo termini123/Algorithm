62b409500441d6257d9dcb5e7fab3b3639fd04fa
termini123 <termini123@naver.com>
2022-07-07 23:10:52 +0900

    14425번 풀이

--- Baekjoon/집합과맵/10815_숫자카드/10815.py
+++ Baekjoon/집합과맵/10815_숫자카드/10815.py
@@ -1,10 +1,22 @@
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+
+def binary_search(num):
+    l = 0
+    r = n-1
+    while l <= r :
+        mid = (l+r)//2
+        if a[mid] == num :
+            return 1
+        elif a[mid] > num :
+            r = mid - 1
+            # 반 줄여주기 1
+        else:
+            l = mid + 1
+            # 반 줄여주기 2
+    return 0
+
 input()
-n = set(map(int, input().split()))
-print(n)
-input()
-m = list(map(int, input().split()))
-for i in m:
-    if i in n:
-        print(1, end=' ')
-    else:
-        print(0, end=' ')
+for num in list(map(int, input().split())):
+    print(binary_search(num), end = ' ')
