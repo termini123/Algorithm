a05ea7ae5d09b64d6316babef4979064111adbe3
termini123 <termini123@naver.com>
2022-08-24 19:00:26 +0900

    15649번 풀이

--- Baekjoon/정수론및조합론/2004_조합0의개수/2004.py
+++ Baekjoon/정수론및조합론/2004_조합0의개수/2004.py
@@ -1,6 +1,28 @@
-def two_count(n):
-    two = 0
-    while n != 0:
-        n = n // 2
-        two += n
-    return two
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+def count2(N):
+    if N < 2:
+        return 0
+    
+    count = 0
+    while N >= 2:
+        count += N//2
+        N = N//2
+    return count
+
+def count5(N):
+    if N < 5:
+        return 0
+    
+    count = 0
+    while N >= 5:
+        count += N//5
+        N //= 5
+    return count
+
+two_count = count2(n) - count2(n-m) - count2(m)
+five_count = count5(n) - count5(n-m) - count5(m)
+print(min(two_count, five_count))
