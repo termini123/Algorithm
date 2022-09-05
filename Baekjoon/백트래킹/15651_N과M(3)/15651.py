d5cdaa97de7d05a567faf025a28a101556feab84
termini123 <termini123@naver.com>
2022-09-05 19:23:44 +0900

    15651번 풀이

---
+++ Baekjoon/백트래킹/15651_N과M(3)/15651.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+out = []
+
+def solve(depth, N, M):
+    if depth == M:
+        print(' '.join(map(str, out)))
+        return
+    for i in range(N):
+        out.append(i+1)
+        solve(depth+1, N, M)
+        out.pop()
+
+solve(0, N, M)
